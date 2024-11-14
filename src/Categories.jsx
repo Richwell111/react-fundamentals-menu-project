bf892ef9faa8c6dbfacfc595ca0ddf336e1981fb
Richwell111 <bcict2013@gmail.com>
2024-11-14 10:43:18 +0000

    Initial commit

---
+++ src/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Categories = ({categories,filterItems}) => {
+  return (
+    <div className='btn-container'>
+        {categories.map((category)=>{
+            return <button type="button" className="btn" key={category} onClick={()=>filterItems(category)}>{category}</button>
+        })}
+    </div>
+  )
+}
